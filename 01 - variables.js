97df950c80060935065a02532fad85915e6af30b
juanblariza <juanblariza@gmail.com>
2021-02-09 19:37:55 -0300

    First commit

---
+++ 01 - variables.js
@@ -0,0 +1,5 @@
+var a = "50";
+var b = Number(a);
+
+console.log(a);
+console.log(b);
---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("Hola Mundo");
+
+const fs = require("fs");
+
+fs.writeFile('filee.txt', 'Hola mundo!!', function (err) {
+    if (err) return console.error(err);
+});
+
+//TODO Otras metodos de escribir archivos y dife con este!
+
+
+let asd;
+const qwe = 'Value';
+
+let numero = 4;
+let cadena = "asd";
+let gm = true;
+let dev = null;
+let unde = undefined;
+let otra = dev;
+
+let ob = {
+    variable: 'test'
+};
+
+let ob2 = ob.variable;
+
+
+function gm2() {
+
+}
